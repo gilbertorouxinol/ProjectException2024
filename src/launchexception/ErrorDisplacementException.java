006348ea87774b25d065c5e7f62eea9fe633cde3
gilbertorouxinol <gilberto.rouxinol@gmail.com>
2024-04-10 20:38:23 +0100

    Credits

--- src/launchexception/ErrorDisplacementException.java
+++ src/launchexception/ErrorDisplacementException.java
@@ -1,5 +1,20 @@
 package launchexception;
 
+/**
+ * IntelliJ IDEA 2021.2.2 (Ultimate Edition)<br>
+ * Licensed to Gilberto Rouxinol<br>
+ * For educational use only.<br><br>
+ * <p>
+ * Polytechnic Institute of Viseu<br>
+ * School of Technology and Management of Viseu<br><br>
+ * <p>
+ * Class created by Gilberto Rouxinol on 2024<br>
+ * Copyright © 2024 Gilberto Rouxinol<br>
+ * All rights reserved<br><br>
+ * <p>
+ * @author Gilberto Rouxinol
+ * @version 2024.04.10
+ */
 public class ErrorDisplacementException extends Exception{
     ErrorDisplacementException(String str){
 
